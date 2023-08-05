2bd3e0272dc42de640b6adf101f041614345f2ef
Beatrice Occhiena <s314971@studenti.polito.it>
2023-08-05 13:02:10 +0200

    fault models first draft

--- src/resilience/fault_models.rs
+++ src/resilience/fault_models.rs
@@ -1,17 +1,33 @@
-// Defines the FaultModel enum, representing the different fault models (stuck-at-0, stuck-at-1, transient bit-flip).
+/* Module for fault models */
+
+use rand::Rng;
+
+// Enum representing the different fault types
 #[derive(Debug, Clone, Copy)]
-pub enum FaultModel {
+enum FaultType {
     StuckAt0,
     StuckAt1,
     TransientBitFlip,
 }
 
-impl std::fmt::Display for FaultModel {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        match self {
-            FaultModel::StuckAt0 => write!(f, "Stuck-at-0"),
-            FaultModel::StuckAt1 => write!(f, "Stuck-at-1"),
-            FaultModel::TransientBitFlip => write!(f, "Transient bit-flip"),
-        }
+// Struct representing a fault occurrence with its properties
+#[derive(Debug, Clone, Copy)]
+struct Fault {
+    component_index: usize,
+    bit_index: usize,
+    fault_type: FaultType,
+}
+
+// Function to generate a fault occurrence
+fn generate_fault(num_components: usize, num_bits_per_component: usize, fault_type: FaultType) -> Fault {
+    
+    let mut rng = rand::thread_rng(); // Initialize random number generator
+    let component_index = rng.gen_range(0..num_components);
+    let bit_index = rng.gen_range(0..num_bits_per_component);
+
+    Fault {
+        component_index,
+        bit_index,
+        fault_type,
     }
 }
