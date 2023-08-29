bba0dbd802be4956eaef7924029630500db30171
Beatrice Occhiena <s314971@studenti.polito.it>
2023-08-29 13:25:19 +0200

    generate random fault method added

--- src/resilience/simulation.rs
+++ src/resilience/simulation.rs
@@ -61,36 +61,8 @@ impl < N: Neuron + Clone + Send + 'static > SNN < N >
 
                 let mut v = Vec::new();
 
-                // If the fault is a transient bit-flip fault
-                // -> Select a random time step from the input sequence
-                let mut time_step: Option<u64> = None;
-                if user_selection.fault_type == FaultType::TransientBitFlip {
-                    time_step = Some(rand::thread_rng().gen_range(0..num_time_steps) as u64);
-                }
-
-                // Select a random component from the list of components
-                let component_index = rand::thread_rng().gen_range(0..user_selection.components.len());
-                let component_type = user_selection.components[component_index];
-
-                // Identify the category of the component
-                let component_category = component_type.get_category();
-
-                // Select a random layer from the list of layers
-                let layer_index = rand::thread_rng().gen_range(0..snn.get_num_layers());
-
-                // Select a random index of the component from the list of components of the given type in the layer
-                let layer = snn.get_layer(layer_index);
-                let num_components = layer.lock().unwrap().get_num_components_from_type(&component_type);
-                let component_index = rand::thread_rng().gen_range(0..num_components);
-
-                // Select a random bit index for the component (not for threshold comparators)
-                let mut bit_index: Option<usize> = None;
-                if component_type != ComponentType::ThresholdComparator {
-                    bit_index = Some(rand::thread_rng().gen_range(0..64));
-                }
-
-                // Create the injected fault object
-                let injected_fault = InjectedFault::new(user_selection.fault_type, time_step, layer_index, component_type, component_category, component_index, bit_index);
+                // Randomly generate the injected fault
+                let injected_fault = Self::generate_random_fault(user_selection.components,user_selection.fault_type, &snn, &num_time_steps);
 
                 for input_spike_train in input_spikes {
                     // Process the input sequence with the injected fault
@@ -117,6 +89,41 @@ impl < N: Neuron + Clone + Send + 'static > SNN < N >
 
         vec_results
             
+    }
+
+    fn generate_random_fault(components: Vec<ComponentType>, fault_type: FaultType,snn: &SNN<N>, num_time_steps: &usize) -> InjectedFault {
+                
+        // If the fault is a transient bit-flip fault
+        // -> Select a random time step from the input sequence
+        let mut time_step: Option<u64> = None;
+        if fault_type == FaultType::TransientBitFlip {
+            time_step = Some(rand::thread_rng().gen_range(0..*num_time_steps) as u64);
+        }
+
+        // Select a random component from the list of components
+        let component_index = rand::thread_rng().gen_range(0..components.len());
+        let component_type = components[component_index];
+
+        // Identify the category of the component
+        let component_category = component_type.get_category();
+
+        // Select a random layer from the list of layers
+        let layer_index = rand::thread_rng().gen_range(0..snn.get_num_layers());
+
+        // Select a random index of the component from the list of components of the given type in the layer
+        let layer = snn.get_layer(layer_index);
+        let num_components = layer.lock().unwrap().get_num_components_from_type(&component_type);
+        let component_index = rand::thread_rng().gen_range(0..num_components);
+
+        // Select a random bit index for the component (not for threshold comparators)
+        let mut bit_index: Option<usize> = None;
+        if component_type != ComponentType::ThresholdComparator {
+            bit_index = Some(rand::thread_rng().gen_range(0..64));
+        }
+
+        // Create and return the injected fault object
+        InjectedFault::new(fault_type, time_step, layer_index, component_type, component_category, component_index, bit_index)
+
     }
 
 }
