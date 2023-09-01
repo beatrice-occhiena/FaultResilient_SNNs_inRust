7465737ad509bf597c5dd027b1a207412822f72f
Beatrice Occhiena <s314971@studenti.polito.it>
2023-09-01 12:04:02 +0200

    new test

--- tests/resilience_test.rs
+++ tests/resilience_test.rs
@@ -1,17 +1,12 @@
-use std::arch::x86_64::_SIDD_POSITIVE_POLARITY;
-
 use group02::network::config::{build_network_from_setup, compute_accuracy, compute_max_output_spike, network_setup_from_file};
 use group02::resilience::components::{ComponentType, ComponentCategory};
 use group02::resilience::fault_models::{FaultType, InjectedFault};
 
 #[test]
-fn test_static_weight_fault_injection() {
+fn test_weight_fault_injection() {
 
     let n = network_setup_from_file();
     let (snn, input_spike_train, targets) = build_network_from_setup(n.unwrap());
-
-    // component value pre fault injection
-    let prev = snn.get_layer(1).lock().unwrap().extra_weights[0][0];
 
     // MANUAL FAULT INJECTION
     //***************************************************************************
@@ -35,18 +30,48 @@ fn test_static_weight_fault_injection() {
     let acc = compute_accuracy(vec_max, &targets);
     println!("Accuracy = {}%", acc);
 
-    // component value post fault injection
-    let post = snn.get_layer(1).lock().unwrap().extra_weights[0][0];
-
     // PRINT RESULTS
     println!(""); // empty line
     println!("Injected fault info:");
     println!("{:?}", fault);
     println!("Resulting accuracy = {}%", acc);
     println!(""); // empty line
-    
-    println!("value pre fault: {:?}", prev);
-    println!("value post fault: {:?}", post);
 
+}
+
+#[test]
+fn test_negative_threshold_fault_injection() {
+
+  let n = network_setup_from_file();
+  let (snn, input_spike_train, targets) = build_network_from_setup(n.unwrap());
+
+  // MANUAL FAULT INJECTION
+  //***************************************************************************
+  let fault_type: FaultType = FaultType::StuckAt1;
+  let time_step: Option<u64> = None;
+  let layer_index: usize = 1;
+  let component_category: ComponentCategory = ComponentCategory::MemoryArea;
+  let component_type: ComponentType = ComponentType::Threshold;
+  let component_index: usize = 0;
+  let bit_index: Option<usize> = Some(63);
+  //***************************************************************************
+  let fault = InjectedFault::new(fault_type, time_step, layer_index, component_type, component_category, component_index, bit_index);
+
+  // PROCESSING WITH FAULT INJECTION
+  let mut vec_max = Vec::new();
+  for input_spikes in input_spike_train.iter() {
+      let output_spikes = snn.process_input(&input_spikes, Some(fault));
+      let max = compute_max_output_spike(output_spikes);
+      vec_max.push(max);
+  }
+  let acc = compute_accuracy(vec_max, &targets);
+  println!("Accuracy = {}%", acc);
+
+  // PRINT RESULTS
+  println!(""); // empty line
+  println!("Injected fault info:");
+  println!("{:?}", fault);
+  println!("Resulting accuracy = {}%", acc);
+  println!(""); // empty line
 
 }
